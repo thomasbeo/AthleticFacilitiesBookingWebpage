e70f9530bd5a1055f7e5ce925ab5f073647bf293
thomasbeo <tbeopoulos@gmail.com>
2026-05-04 18:22:08 +0300

    Deploy

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,21 +3,21 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER,       // your gmail
+    pass: process.env.EMAIL_PASS        // app password
   }
 });
 
-// Γενική συνάρτηση αποστολής email
 async function sendEmail({ to, subject, text, html }) {
   try {
-    let info = await transporter.sendMail({
+    const info = await transporter.sendMail({
       from: `"Facilities Booking" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
       html
     });
+
     console.log("✅ Email sent:", info.messageId);
   } catch (err) {
     console.error("❌ Error sending email:", err);
